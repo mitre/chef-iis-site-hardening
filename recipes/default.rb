3589dc0463f7f7ae5f5a1e8b3a9fd554f8149eea
HackerShark <melsharkawi@mitre.org>
2018-11-01 12:57:16 -0400

    added iis cookbook to our hardening cookbook
    Signed-off-by: HackerShark <melsharkawi@mitre.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,32 +4,6 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-powershell_script 'V-76773' do
-  code <<-EOH
-  Set-WebConfigurationProperty -filter "system.applicationHost/sites/siteDefaults/limits" -name MaxConnections -value 4294967295
-  EOH
-end
 
-powershell_script 'V-76775' do
-  code <<-EOH
-  Set-WebConfigurationProperty -filter "system.web/sessionState" -name mode -value InProc
-  EOH
-end
-
-powershell_script 'V-76777' do
-  code <<-EOH
-  Set-WebConfigurationProperty -filter "system.web/sessionState" -name cookieless -value UseCookies
-  EOH
-end
-
-powershell_script 'V-76779, V-76781, V-76809' do
-  code <<-EOH
-  Set-WebConfigurationProperty -filter "system.webServer/security/access" -name sslFlags -value 'Ssl,SslRequireCert'
-  EOH
-end
-
-powershell_script 'V-76805' do
-  code <<-EOH
-  Set-WebConfigurationProperty -filter "system.web/trust" -name level -value Full
-  EOH
-end
+include_recipe 'iis::default'
+include_recipe 'chef_iis_85_hardening::services_and_features'
